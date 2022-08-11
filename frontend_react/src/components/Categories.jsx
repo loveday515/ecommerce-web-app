3347d0f8a1d301bc93a38f936873dea219656ecf
loveday515 <lovedayikegbulam515@gmail.com>
2022-08-11 05:09:47 +0100

    update slider and create new component

---
+++ frontend_react/src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+import {Categories} from "../data"
+
+const Container = styled.div``
+
+const Categories = () => {
+  return (
+    <Container>Categories</Container>
+  )
+}
+
+export default Categories
