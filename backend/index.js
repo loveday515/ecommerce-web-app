2210520bcbd10322d3b85ff3e3acd8156838ff7a
loveday515 <lovedayikegbulam515@gmail.com>
2022-09-04 20:01:13 +0100

    add routes

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const userRoute = require("./routes/user");
+
+const port = process.env.PORT || 5000;
+
+mongoose
+	.connect(`${process.env.MONGO_URL}`)
+	.then(() => {
+		console.log("DB Connection Successful");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+app.use(express.json());
+
+app.use("/api/v0/users", userRoute);
+
+app.listen(`${port}`, (err) => {
+	if (err) {
+		console.log("ERROR", err);
+	}
+	console.log(`backend server started successfully on ${port}`);
+});
