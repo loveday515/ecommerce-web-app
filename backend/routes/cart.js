834c22dba96d56860f5a92e68943a7c796814fec
loveday515 <lovedayikegbulam515@gmail.com>
2022-09-09 23:17:35 +0100

    update cart route

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,3 +1,71 @@
 const router = require("express").Router();
+const Cart = require("../models/Cart");
+const {
+	verifyTokenAndAdmin,
+	verifyTokenAndAuthourization,
+	verifyToken,
+} = require("./verifyToken");
+
+// CREATE CART
+router.post("/", verifyToken, async (req, res) => {
+	const newCart = new Cart(req.body);
+
+	try {
+		const savedCart = await newCart.save((error) => {
+			if (error) {
+				console.log(error);
+			}
+		});
+		res.status(200).json(savedCart);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+//UPDATE CART
+router.put("/:id", verifyTokenAndAuthourization, async (req, res) => {
+	try {
+		const updatedCart = await Cart.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.status(200).json(updatedCart);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//DELETE CART
+router.delete("/:id", verifyTokenAndAuthourization, async (req, res) => {
+	try {
+		await Cart.findByIdAndDelete(req.params.id);
+		res.status(200).json("Cart has been deleted");
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// GET USER CART
+router.get("/find/:userId", verifyTokenAndAuthourization, async (req, res) => {
+	try {
+		const cart = await Cart.findOne(req.params.id);
+		res.status(200).json(cart);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// GET ALL PRODUCTS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+	try {
+		const carts = await Cart.find();
+		res.status(200).json(carts);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 module.exports = router;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -47,7 +47,7 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 // GET PRODUCT
 router.get("/find/:id", async (req, res) => {
 	try {
-		const product = await Product.findByIdAndDelete(req.params.id);
+		const product = await Product.findById(req.params.id);
 		res.status(200).json(product);
 	} catch (error) {
 		res.status(500).json(error);
